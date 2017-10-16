7055851fc6ae42d802ca8d7b00349b7ca0ebe11a
TIAGO FELICIANO GOMES <u15193@academico.cotuca.unicamp.br>
2017-10-16 12:07:48 -0200

    O que a NASA não quer que você veja

---
+++ Assets/Assets/Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLookAtTarget : MonoBehaviour {
+
+    void Start() {
+	}
+
+	// Called when MouseDown on this gameObject
+	void OnMouseDown () {
+
+        // change the target of the LookAtTarget script to be this gameobject.		
+        desativarTodos();
+        GameObject.Find(this.name).GetComponent<AudioSource>().enabled = true;
+        //Camera.main.fieldOfView = 60 * target.transform.localScale.x;
+    }
+
+    void desativarTodos()
+    {
+        GameObject[] vetPlanetas = GameObject.FindGameObjectsWithTag("tudo");
+
+        for (int i = 0; i < vetPlanetas.Length; i++)
+        {
+            Debug.Log(vetPlanetas[i].name + " parar");
+            vetPlanetas[i].GetComponent<AudioSource>().enabled = false;
+        }
+    }
+}
